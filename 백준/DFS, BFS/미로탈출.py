d39bc263dcd63f827abfe6b66c227103c9fa6eae
namho.kim <shawnbest@naver.com>
2026-01-27 23:11:31 +0900

    add BFS

--- 백준/DFS, BFS/미로탈출.py
+++ 백준/DFS, BFS/미로탈출.py
@@ -8,7 +8,7 @@ def bfs(x, y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            if nx < 0 or nx >= n or ny < 0  or ny >= m:
                 continue
             if miro[nx][ny] == 1:
                 miro[nx][ny] = miro[x][y] + 1
