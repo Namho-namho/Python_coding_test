6a0874e9bebbbc30b9fcf09db5f95f42f51b91e0
namho.kim_deskTop <shawnbest@naver.com>
2026-02-08 22:40:36 +0900

    binary seatch basic

--- 백준/이진 탐색/note.py
+++ 백준/이진 탐색/note.py
@@ -6,9 +6,9 @@ def binary_search1(array, target, start, end):
     if array[mid] == target:
         return mid
     elif array[mid] > target:
-        binary_search1(array, target, start, mid - 1)
+         return binary_search1(array, target, start, mid - 1)
     else:
-        binary_search1(array, target, mid + 1, end)
+        return binary_search1(array, target, mid + 1, end)
 
 # 2 반복
 def binary_search2(array, target, start, end):
