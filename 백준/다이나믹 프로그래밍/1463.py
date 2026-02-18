b414fd1993d9e84003e11448f7d4a03eb79ea8bd
namho.kim_deskTop <shawnbest@naver.com>
2026-02-18 23:49:58 +0900

    바텀업 위주로 생각하기 로직

---
+++ 백준/다이나믹 프로그래밍/1463.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+# ai = min(ai-1, ai/2, ai/3) + 1
+
+d = [0] * (n + 1)
+
+#bottom up
+for i in range(2, n + 1):
+    d[i] = d[i - 1] 
+    if i % 2 == 0: 
+        d[i] = min(d[i-1], d[i//2]) + 1
+    if i % 3 == 0:
+        d[i] = min(d[i-1], d[i//3]) + 1
+
+print(d[n])
