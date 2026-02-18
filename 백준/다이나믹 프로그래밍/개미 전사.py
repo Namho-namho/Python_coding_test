68b96a54946124efdeb62d899b67a001b66710a1
namho.kim_deskTop <shawnbest@naver.com>
2026-02-18 23:21:18 +0900

    다이나믹 기본, 리스트를 만들어놓고 차근차근 쌓는 로직

---
+++ 백준/다이나믹 프로그래밍/개미 전사.py
@@ -0,0 +1,13 @@
+N = int(input())
+K = list(map(int, input().split()))
+
+# ai = max(ai-1, ai-2 + ki)
+
+d = [0] * 100
+
+d[0] = K[0]
+d[1] = max(K[0], K[1])
+for i in range(2, N):
+    d[i] = max(d[i-1], d[i-2] + K[i])
+
+print(d[N - 1])
