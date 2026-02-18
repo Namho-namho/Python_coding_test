db9759861e9dc101cfa4ed9051bb70f129ec65c0
namho.kim_deskTop <shawnbest@naver.com>
2026-02-18 23:55:48 +0900

    mistake

--- 백준/다이나믹 프로그래밍/1463.py
+++ 백준/다이나믹 프로그래밍/1463.py
@@ -8,10 +8,10 @@ d = [0] * (n + 1)
 
 #bottom up
 for i in range(2, n + 1):
-    d[i] = d[i - 1] 
+    d[i] = d[i - 1] + 1
     if i % 2 == 0: 
-        d[i] = min(d[i-1], d[i//2]) + 1
+        d[i] = min(d[i], d[i//2] + 1)
     if i % 3 == 0:
-        d[i] = min(d[i-1], d[i//3]) + 1
+        d[i] = min(d[i], d[i//3] + 1)
 
 print(d[n])
